0aee974ecd46da9064020f6fcbdc2156a21a302f
dp9212 <dp9212@163.com>
2017-06-29 21:50:33 +0800

    测试提交

--- src/main/java/com/dp/photo/web/controller/MainController.java
+++ src/main/java/com/dp/photo/web/controller/MainController.java
@@ -5,23 +5,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
+ * 公共
  * Created by dengpeng on 2017/6/29.
  */
 @Controller
 @RequestMapping
 public class MainController {
 
+    /* 主页 */
     @GetMapping("/index")
     public String index(){
         return "index";
     }
 
+    /* 默认跳转登录 */
     @GetMapping
     public String main(){
         return "login";
     }
+    /* 登录 */
     @GetMapping("/login")
     public String login(){
         return "login";
     }
+
 }
